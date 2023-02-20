844a78bf876eab20c1a6e2c1d23de579b7285402
Elviiss <elvis22elmejor@gmail.com>
2023-02-20 15:39:18 +0100

    creando archivo json

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+
+const json_books = fs.readFileSync('./books.json', 'utf-8');
+let booksjson = JSON.parse(json_books);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +13,22 @@ router.get('/', function(req, res, next) {
 
 router.get('/newbooks', function(req, res, next) {
   res.render('addbooks', { title: 'Add new books' });
+});
+
+router.post('/newbooks', (req, res, next) => {
+  const { titulo, imagen, descripcion } = req.body;
+  let newBook = {
+    titulo: titulo,
+    imagen: imagen,
+    descripcion: descripcion
+  }
+  booksjson.push(newBook);
+  console.log(newBook)
+
+  const json_books = JSON.stringify(booksjson)
+
+  fs.writeFileSync('./books.json', json_books, 'utf-8');
+  res.redirect("/")
 });
 
 router.get('/books', function(req, res, next) {
